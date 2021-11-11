74c62b03a3d50c75fe98866e0792b54affd70b51
Chris Ciszak <chris.ciszak@gmail.com>
2021-11-11 19:09:01 +0000

    all genes searchable

--- indexer/scripts/buildData.ts
+++ indexer/scripts/buildData.ts
@@ -96,7 +96,7 @@ class Ethereum {
   async fetchContracts() {
     console.log(this.contractAddresses);
     const contractsByNetwork: ContractsResponse = await fetch(
-      'https://raw.githubusercontent.com/KryptomonDAO/contracts/master/addresses.json?token=ACWWO7KNPVDOQ3Q3UZTREKLBD2XHY'
+      'https://raw.githubusercontent.com/KryptomonDAO/contracts/master/addresses.json?token=ACWWO7JWR5HC3IM24QPRIITBRUQQM'
     )
     this.contractAddresses = contractsByNetwork[this.network]
   }
