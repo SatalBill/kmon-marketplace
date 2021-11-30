3fd3c7406aaaf3d8c7189b5e54e4e10667561a2f
velopace <velopace@outlook.com>
2021-11-29 19:45:47 -0600

    Bug fix in acceptbutton and warningmessage

--- webapp/src/components/Bid/WarningMessage/WarningMessage.tsx
+++ webapp/src/components/Bid/WarningMessage/WarningMessage.tsx
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { t } from '@kmon/dapps/dist/modules/translation/utils'
+import { Coin } from '@kmon/schemas'
 
 import { useFingerprint } from '../../../modules/nft/hooks'
 import {
-  isInsufficientKMON,
+  isInsufficientCoin,
   checkFingerprint
 } from '../../../modules/bid/utils'
 import { Props } from './WarningMessage.types'
 import './WarningMessage.css'
+import { getContractNames } from '../../../modules/vendor'
+import { getContract } from '../../../modules/contract/utils'
 
 const WarningMessage = (props: Props) => {
   const { nft, bid } = props
 
   const [fingerprint] = useFingerprint(nft)
-  const [hasInsufficientKMON, setHasInsufficientKMON] = useState(false)
+  const [hasInsufficientCoin, setHasInsufficientCoin] = useState(false)
+
+  const contractNames = getContractNames()
+
+  const { address: kmonAddress } = getContract({
+    name: contractNames.KMONToken
+  })
+  const { address: wbnbAddress } = getContract({
+    name: contractNames.WBNB
+  })
+
+  let coin = Coin.KMON
+  if (bid.paymentToken === kmonAddress) {
+    coin = Coin.KMON
+  } else if (bid.paymentToken === wbnbAddress) {
+    coin = Coin.WBNB
+  } else {
+    throw new Error(`Invalid payment token in a Bid ${bid}`)
+  }
 
   useEffect(() => {
-    isInsufficientKMON(bid)
-      .then(setHasInsufficientKMON)
+    isInsufficientCoin(bid, coin)
+      .then(setHasInsufficientCoin)
       .catch(error =>
-        console.error(`Could not get the KMON from bidder ${bid.bidder}`, error)
+        console.error(`Could not get the ${coin} from bidder ${bid.bidder}`, error)
       )
   }, [bid])
 
   const isValidFingerprint = checkFingerprint(bid, fingerprint)
 
-  if (hasInsufficientKMON) {
+  if (hasInsufficientCoin) {
     return (
       <div className="WarningMessage">
-        {t('bid.not_enough_mana_on_bid_placed')}
+        {t('bid.not_enough_coin_on_bid_placed', { coin })}
       </div>
     )
   }
