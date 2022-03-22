244848d285eff567d2f539b6b7da6d239bee107f
velopace <velopace@outlook.com>
2022-03-22 12:14:46 -0500

    Fix toast issue

--- webapp/src/modules/toast/sagas.ts
+++ webapp/src/modules/toast/sagas.ts
@@ -75,15 +75,19 @@ function isUserDeniedSignature(message: string) {
 function* handleBreedFailure(action: BreedFailureAction) {
   const { error } = action.payload
   // @ts-ignore
-  if (error && error.message) {
+  if (error && error.data && error.data.message) {
     // @ts-ignore
-    yield put(showToast(getBreedFailureToast(error.message)))
-  // @ts-ignore
+    yield put(showToast(getBreedFailureToast(error.data.message)))
+    // @ts-ignore
   } else if (error && error.error && error.error.message) {
     // @ts-ignore
     yield put(showToast(getBreedFailureToast(error.error.message)))
-  }
-  else {
+    // @ts-ignore
+  } else if (error && error.message) {
+    // @ts-ignore
+    yield put(showToast(getBreedFailureToast(error.message)))
+  // @ts-ignore
+  } else {
     yield put(showToast(getBreedFailureToast("Something went wrong")))
   }
 }
