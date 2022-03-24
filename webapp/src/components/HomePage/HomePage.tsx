f277a4a97698a77be255dc6bf471d7e119e15732
TC <99806368+YoYoYosko@users.noreply.github.com>
2022-03-25 00:44:43 +0100

    Hero Logo redirect

--- webapp/src/components/HomePage/HomePage.tsx
+++ webapp/src/components/HomePage/HomePage.tsx
@@ -69,7 +69,7 @@ const HomePage = (props: Props) => {
         <Hero.Content>
           <div className="hero-image" />
           <div className="hero-fade" />
-          <div className="hero-logo" />
+          <a className="hero-logo" href="https://kryptomon.co"/>
           <div className="hero-market" />
           <div className="grid-top" />
           <div className="dragons" />
@@ -82,8 +82,9 @@ const HomePage = (props: Props) => {
       </Hero>
       
       <div className="HomePageContent">
-        <Navigation />
+      <Navigation />
         <Page className="HomePage">
+        
           {views.map((view, index) => {
             return (
               <>
