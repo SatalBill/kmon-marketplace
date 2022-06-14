21aaf1beafc0d15563bdc959376c00f5a2e85edc
SatalBill <satel1217@163.com>
2022-06-14 23:41:49 +0800

    external link added on community card

--- webapp/src/components/HomePage/Components/Community.tsx
+++ webapp/src/components/HomePage/Components/Community.tsx
@@ -5,17 +5,18 @@ import { facebook, medium, telegram, linkedin, twitter, discord, reddit, instagr
 // type Props = {}
 
 const communityData = [
-    { id: 1, icon: facebook, amount: 58, unit: 'followers' },
-    { id: 2, icon: medium, amount: 2.7, unit: 'followers' },
-    { id: 3, icon: telegram, amount: 100, unit: 'members' },
-    { id: 4, icon: linkedin, amount: 10, unit: 'followers' },
-    { id: 5, icon: twitter, amount: 132, unit: 'followers' },
-    { id: 6, icon: discord, amount: 61, unit: 'members' },
-    { id: 7, icon: reddit, amount: 26, unit: 'trainers' },
-    { id: 8, icon: instagram, amount: 16, unit: 'followers' },
+    { id: 1, icon: facebook, amount: 58, unit: 'followers', link: 'https://www.facebook.com/KryptomonTeam/' },
+    { id: 2, icon: medium, amount: 2.7, unit: 'followers', link: 'https://medium.com/kryptomon' },
+    { id: 3, icon: telegram, amount: 100, unit: 'members', link: 'https://t.me/kryptomonofficial' },
+    { id: 4, icon: linkedin, amount: 10, unit: 'followers', link: 'https://www.linkedin.com/company/the-kryptomon-company' },
+    { id: 5, icon: twitter, amount: 132, unit: 'followers', link: 'https://twitter.com/kryptomonteam' },
+    { id: 6, icon: discord, amount: 61, unit: 'members', link: 'https://discord.com/invite/hYRjSfsWXt' },
+    { id: 7, icon: reddit, amount: 26, unit: 'trainers', link: 'https://www.reddit.com/r/Kryptomon/' },
+    { id: 8, icon: instagram, amount: 16, unit: 'followers', link: 'https://www.instagram.com/kryptomonteam' }
 ]
 
 const Community = () => {
+    const openExternalLink = (link: string) => window.open(link, "_blank")
     return (
         <div className="community-container">
             <div className="community-title">
@@ -28,7 +29,7 @@ const Community = () => {
             <div className="card-list">
                 {
                     communityData.map((item) => (
-                        <div className="community-card">
+                        <div key={item.id} className="community-card" onClick={() => openExternalLink(item.link)}>
                             <img className="community-icon" src={item.icon} alt={`${item.icon}`} />
                             <p className="community-followers">{item.amount}K {item.unit}</p>
                         </div>
